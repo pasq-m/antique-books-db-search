5ae0dd95da05daa7ee813ce2fed65dc4cd1129fc
pasq-m <mos.pasquini@gmail.com>
2021-05-26 12:59:28 +0200

    - Added package "com.github.util" - Added "protected" modifier to the variables - Removes unused imports

--- src/com/github/util/GatherBook.java
+++ src/com/github/util/GatherBook.java
@@ -1,43 +1,30 @@
-//package main;
+package com.github.util;
 
-import java.io.IOException;
-import java.net.MalformedURLException;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-import com.gargoylesoftware.htmlunit.BrowserVersion;
-import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
-import com.gargoylesoftware.htmlunit.WebClient;
-import com.gargoylesoftware.htmlunit.html.HtmlPage;
-import com.gargoylesoftware.htmlunit.html.HtmlDivision;
-import com.gargoylesoftware.htmlunit.html.HtmlAnchor;
-import com.gargoylesoftware.htmlunit.html.DomNode;
-import com.gargoylesoftware.htmlunit.html.DomNodeList;
 
 public class GatherBook {
 	
-	int x = 0;					//Used as a counter in "AbeBooksSub".
-	int x2 = 1;					//Used as a counter in "EbaySoldSub" and "EbaySoldMapSub".
-	int w = 2;					//Used in the "AbeBooksSub" class for now.
-    int y = 1;					//Used in the "EbaySoldSub" and "EbaySoldMapSub" classes for now.
-    int z = 0;					//For "megalist" List index counter.
+	protected int x = 0;					//Used as a counter in "AbeBooksSub".
+	protected int x2 = 1;					//Used as a counter in "EbaySoldSub" and "EbaySoldMapSub".
+	protected int w = 2;					//Used in the "AbeBooksSub" class for now.
+	protected int y = 1;					//Used in the "EbaySoldSub" and "EbaySoldMapSub" classes for now.
+	protected int z = 0;					//For "megalist" List index counter.
     
-    static String title;
-    static String year;
-    static int yearInt;			//Declared var. for converted values from string to int.
-    static String publisher;
-    static String dateRange;
-    static String format;
-    static String originalOrReprod;
-    static String printTech;
-    static String type;
-    static String priceTagNotAuction;			//Used for not-auction websites book's tag.
-    static String auctionSoldAt;				//Used just for Ebay related books.
+	protected static String title;
+	protected static String year;
+	protected static int yearInt;			//Declared var. for converted values from string to int.
+	protected static String publisher;
+	protected static String dateRange;
+	protected static String format;
+	protected static String originalOrReprod;
+	protected static String printTech;
+	protected static String type;
+	protected static String priceTagNotAuction;			//Used for not-auction websites book's tag.
+	protected static String auctionSoldAt;				//Used just for Ebay related books.
     
-    static double priceTagNotAuctionDoub;		//Declared vars. for converted values from string to double.
-    static double auctionSoldAtDoub;			//Declared vars. for converted values from string to double.
-    static double d1;							//"d1" is the result of the average calculation provided by the class "PriceAvgCalc".
+	protected static double priceTagNotAuctionDoub;		//Declared vars. for converted values from string to double.
+	protected static double auctionSoldAtDoub;			//Declared vars. for converted values from string to double.
+	protected static double d1;							//"d1" is the result of the average calculation provided by the class "PriceAvgCalc".
     
 	ArrayList<String[]> superList;
 	public void getElements(String url) throws Exception {	//Method to extract raw data from the website(s).
